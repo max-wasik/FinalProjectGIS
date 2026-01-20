ee1a5d037d1afea85f7ddffa9792f2016c9995f9
JoPlank <joseph.plank@edu.uni-graz.at>
2026-01-20 10:23:42 +0100

    Export bearbeitet

--- DataExport.js
+++ DataExport.js
@@ -14,36 +14,6 @@ zoomControl.addTo(map);
 
 let drawnItems = L.featureGroup().addTo(map); 
 
-// Load example data. Not useful in your projects
-async function loadExampleData(){
-  let loadedData = await fetch('data_example.json')
-  let data = await loadedData.json();
-  console.log(data);
-
-  data.forEach(function(feature) {
-    let color = (feature.sentiment === 'like') ? 'blue' : 'orange'; 
-    let newMarker = L.circleMarker([feature.lat, feature.lng], {
-            color: color,
-            fillColor: color,
-            fillOpacity: 0.5,
-            radius: 8
-        });
-    // add data to the marker: 
-    newMarker.feature = {
-        "type": "Feature",
-        "properties": {
-            "sentiment": feature.sentiment,
-            "comment": feature.comment
-        }
-    };
-    newMarker.bindPopup(feature.comment);
-    drawnItems.addLayer(newMarker);
-  });
-
-}
-loadExampleData();
-
-
 function saveToFile(content, fileName) {
     // Create a "Blob" (A file-like object of immutable raw data)
     let blob = new Blob([JSON.stringify(content, null, 2)], { type: "application/json" });
